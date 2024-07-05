75bfb5845db4c64f4e51a1a9b1fa22e591fb5cc8
Giyeon Kim <gykim@bcs.cmbp.home.arpa>
2024-07-05 23:46:23 +0900

    fs from new

--- calc_fs_4.jl
+++ calc_fs_4.jl
@@ -1,6 +1,8 @@
+include("lurITensorTools.jl")
+
 home_dir = "/home/gykim"
 data_dir = "$(home_dir)/liom/data"
-fs_calc_dir = "$(home_dir)/liom/edv2_make/peng_fs_actual"
+ed_dir = "$(home_dir)/liom/edv2_make"
 
 function main()
 
@@ -8,34 +10,48 @@ function main()
 	syssize = parse(Int64, ARGS[1])
 	niter = parse(Int64, ARGS[2])
 	anum = parse(Int64, ARGS[3])
-	ssite = parse(Int64, ARGS[4])
-	esite = parse(Int64, ARGS[5])
+	makenew = parse(Int64, ARGS[4]) # new : 1, use original : 0
+	ssite = parse(Int64, ARGS[5])
+	esite = parse(Int64, ARGS[6])
 	ls = [0]
 	ws = [0.2]
-	Ws = [3.0, 4, 5, 6, 7, 8, 9, 10, 12, 15, 18, 21, 25, 30]
+	Ws = [12.0, 15.0, 18.0]
 
 	for l in ls
 		for W in Ws
 			for w in ws
 				dis_dir = "$(data_dir)/p4_l$(l)_w$(w)_L$(syssize-l)_W$(W)"
 				println(dis_dir)
+				(!isdir(dis_dir)) && (mkdir(dis_dir))
 
 				Threads.@threads for i=1:nthr*niter
 					result_dir = "$(dis_dir)/r$(i)"
+					(!isdir(result_dir)) && (mkdir(result_dir))
 
-					f = open("$(result_dir)/disorder", "r")
-					disorder = readlines(f)
+					if makenew == 1
+						f = open("$(result_dir)/disorder", "w")
+						fs_calc_dir = "$(ed_dir)/peng_tvf_actual"
+						disorder = build_disorder(l, w, syssize-l, W)
+						for d in disorder
+							println(f, d)
+						end
+					else
+						f = open("$(result_dir)/disorder", "r")
+						fs_calc_dir = "$(ed_dir)/peng_fs_actual"
+						disorder = readlines(f)
+					end
+					close(f)
 					cmd = `$(fs_calc_dir) $(length(disorder)) $disorder $(result_dir) $(anum) $(ssite) $(esite)`
 					run(cmd)
-					println("Completed for r=$(result_dir)")
+					println("Completed for $(result_dir)")
 				end
 			end
 		end
 	end
 end
 
-if length(ARGS) != 5
-	println("Usage : julia --threads <# of threads> calc_fs.jl <L> <# of iterations> <anum> <ssite> <esite>")
+if length(ARGS) != 6
+	println("Usage : julia --threads <# of threads> calc_fs.jl <L> <# of iterations> <anum> <new?1:0> <ssite> <esite>")
 	exit(1)
 end
 main()
--- calc_fs_7.jl
+++ calc_fs_7.jl
@@ -1,12 +1,14 @@
+include("lurITensorTools.jl")
+
 home_dir = "/home/gykim"
 data_dir = "$(home_dir)/liom/data"
-fs_calc_dir = "$(home_dir)/liom/edv2_make/peng_fs_actual"
+ed_dir = "$(home_dir)/liom/edv2_make"
 
 function main()
-
 	nthr = Threads.nthreads()
 	niter = parse(Int64, ARGS[1])
 	anum = parse(Int64, ARGS[2])
+	makenew = parse(Int64, ARGS[3])
 	size_configs = [(4, 4, 4)]
 	ws = [0.2]
 	Ws = [3.0, 4, 5, 6, 7, 8, 9, 10, 12.0, 15.0, 18.0, 21.0, 25.0, 30.0]
@@ -16,14 +18,26 @@ function main()
 		for W in Ws
 			for w in ws
 				dis_dir = "$(data_dir)/p7_l$(l)_w$(w)_L$(L)_M$(M)_W$(W)"
+				(!isdir(dis_dir)) && (mkdir(dis_dir))
 				syssize = L + l + M
 				println(dis_dir)
 
 				Threads.@threads for i=1:nthr*niter
 					result_dir = "$(dis_dir)/r$(i)"
+					(!isdir(result_dir)) && (mkdir(result_dir))
 
-					f = open("$(result_dir)/disorder", "r")
-					disorder = readlines(f)
+					if makenew == 1
+						f = open("$(result_dir)/disorder", "w")
+						fs_calc_dir = "$(ed_dir)/peng_tvf_actual"
+						disorder = build_disorder(l, w, L, M, W)
+						for d in disorder
+							println(f, d)
+						end
+					else
+						f = open("$(result_dir)/disorder", "r")
+						fs_calc_dir = "$(ed_dir)/peng_fs_actual"
+						disorder = readlines(f)
+					end
 					ssite, esite = L + l, syssize - 1
 					cmd = `$(fs_calc_dir) $(length(disorder)) $disorder $(result_dir) $(anum) $(ssite) $(esite)`
 					run(cmd)
@@ -34,8 +48,8 @@ function main()
 	end
 end
 
-if length(ARGS) != 2
-	println("Usage : julia --threads <# of threads> calc_fs_7.jl <# of iterations> <anum>")
+if length(ARGS) != 3
+	println("Usage : julia --threads <# of threads> calc_fs_7.jl <# of iterations> <anum> <new?1:0>")
 	exit(1)
 end
 main()
